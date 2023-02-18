3fe2d323c7a406dee6232d37f7862299d2392d33
friendlystone <georgiana.bumbar@yahoo.com>
2023-02-18 02:01:41 +0200

    Added the configuration file

---
+++ Weather-Station/src/test/java/com/example/Weather/Station/WeatherStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Weather.Station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
